345f2cb192325e1390fc7777fe01ded5284d99e2
Tatiana <tmv0001@mail.ru>
2023-03-19 16:33:54 +0500

    Initianal commit

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,18 @@ public class Main {
     public static void main(String[] args) {
         int check = 100;
         int replenishment = 1200;
-        int bonus_for_rep = 0;
+        int bonusForRep = 0;
         int total;
 
 
         if (replenishment > 1000) {
-            bonus_for_rep = (replenishment / 100) * 1;
-            total = bonus_for_rep + replenishment + check;
+            bonusForRep = (replenishment / 100) * 1;
+            total = bonusForRep + replenishment + check;
         }
         System.out.println("Бонусов начислено:");
-        System.out.println(bonus_for_rep);
+        System.out.println(bonusForRep);
         System.out.println("Сумма на счете:");
-        System.out.println(bonus_for_rep + replenishment + check);
+        System.out.println(bonusForRep + replenishment + check);
 
     }
 }
